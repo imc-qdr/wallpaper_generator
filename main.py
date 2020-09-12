5900d3baae726e3750463fc3781ad98f5206dedc
Abdulkader <19IMC10609@fh-krems.ac.at>
2020-09-12 22:59:51 +0200

    added a system tray feature

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from threading import Timer
+from pystray import Icon, Menu, MenuItem
 from random import choice
+from time import ctime, sleep
+import threading
 import quotes
 import image
 import os
 import ctypes
+
 
 pics_topics = ['nature', 'galaxy', 'night sky',
                'peaceful', 'ocean waves', 'cave',
@@ -14,6 +17,7 @@ authors = ['carl-jung', 'alan-watts', 'marcus-aurelius',
            'lao-tzu', 'oscar-wilde', 'viktor-e-frankl',
            'joseph-campbell', 'lewis-carroll', 'friedrich-nietzsche',
            'mark-twain', 'albert-camus', 'socrates']
+run = True
 
 
 def change_wallpaper():
@@ -22,9 +26,32 @@ def change_wallpaper():
     quote_with_pic = image.add_quote(pic, quotes.brainy_quotes_specific(choice(authors)))
     quote_with_pic.save(path, "JPEG")
     ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
-    print("operation successful")
-    Timer(40, change_wallpaper).start()
+    print(f'operation successful: {ctime()}')
+
+
+def create_tray():
+    tray = Icon('wisdom on pic', image.Image.open('icon.png'),
+                menu=Menu(MenuItem('new image', change_wallpaper),
+                          MenuItem('exit', terminate)))
+    return tray
+
+
+def on_schedule():
+    while run:
+        change_wallpaper()
+        sleep(40)
+    return
+
+
+def terminate():
+    icon.stop()
+    global run
+    run = False
 
 
 if __name__ == '__main__':
-    change_wallpaper()
+    thread = threading.Thread(target=on_schedule)
+    thread.start()
+    icon = create_tray()
+    icon.run()
+    thread.join()
